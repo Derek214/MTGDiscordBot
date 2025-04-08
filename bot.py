26d7b5922afa83b2876beeb496c1ffeb557bd63f
needhambg <needhambg@etsu.edu>
2025-04-08 19:22:04 -0400

    Added Card Channel Watcher

--- bot.py
+++ bot.py
@@ -1,9 +1,14 @@
 import os
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv() 
 TOKEN = os.getenv("DISCORD_TOKEN") 
+
+bot = commands.Bot(command_prefix='/', intents=discord.Intents.default())
+
+CARD_CHANNEL_ID = 1359304823903748156
 
 class MyClient(discord.Client):
     async def on_ready(self):
@@ -20,3 +25,16 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 client = MyClient(intents=intents)
+
+
+@bot.event
+async def on_message(message):
+    if message.channel.id != CARD_CHANNEL_ID:
+        return
+    
+    if message.author == bot.user:
+        return
+
+    if message.content.startswith('/card'):
+        await message  #API call to get card data
+        await message.channel.send('Card data here!')
